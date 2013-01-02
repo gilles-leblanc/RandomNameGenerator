1bc5c56c725d3d0c9c758dfe9c3e663b8f3dd9a5
Gilles Leblanc <gilles.leblanc@gmail.com>
2013-01-02 10:55:23 -0500

    first commit

---
+++ probability_table.rb
@@ -0,0 +1,25 @@
+require "yaml"
+
+# Represents the probability of a group of letters composed of 1..* letters of being followed by another letter N.
+class ProbabilityTable
+  attr_reader :frequencies
+
+  def initialize(letter_length_strategy, input_loader)
+    @frequencies = Hash.new
+    @letter_length_strategy = letter_length_strategy
+    @input_loader = input_loader
+  end
+
+  def empty?
+    @frequencies.empty?
+  end
+
+  def load(input_name)
+    input = @input_loader.load(input_name)
+    @frequencies = @letter_length_strategy.create_frequencies(input)
+  end
+
+  def to_file(filename = 'ProbabilityTable')
+    File.open(filename, "w") {|file| file.puts YAML::dump(@frequencies)}
+  end
+end
---
+++ random_name_generator_test.rb
@@ -0,0 +1,8 @@
+require_relative 'random_name_generator'
+
+class RandomNameGeneratorTest
+  generator = RandomNameGenerator.new("media/greek_myth_sample")
+
+  puts "Generating 40 names"
+  40.times {puts generator.generate}
+end
